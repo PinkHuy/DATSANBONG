678a8536a840e0d00d765267bcc8d5e6df02b337
nguyenthanhco0402-sys <nguyenthanhco0402@gmail.com>
2026-05-23 22:44:09 +0700

    fullcode

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,29 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-
+using DATSANBONG.Data;
 using DATSANBONG.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace DATSANBONG.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly DataClasses1DataContext db = new DataClasses1DataContext("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=QuanLySanBong_MVC;Integrated Security=True;MultipleActiveResultSets=True");
+        private readonly QuanLySanBongDb _db = new QuanLySanBongDb();
 
-        // HomeController.cs
         public ActionResult Index()
         {
-            // ✅ Dùng subquery an toàn hơn cho LINQ to SQL
-            var sanNoiBat = (from s in db.SanBongs
-                             where s.TrangThai == "Hoạt động" || s.TrangThai == "Sẵn sàng"
-                             let soLuotDat = db.DatSans
-                                 .Count(d => d.MaSan == s.MaSan && d.TrangThai != "Đã hủy")
-                             orderby soLuotDat descending
-                             select s)
-                            .Take(3)
-                            .ToList();
+            // Lấy tất cả sân hoạt động qua ADO.NET (không dùng LINQ to SQL)
+            List<SanBong> tatCaSan = _db.LayDanhSachSanBong(chiHoatDong: true);
+
+            // Lấy top 3 sân có nhiều lượt đặt nhất
+            var tatCaDatSan = _db.LayTatCaDatSan();
+
+            var sanNoiBat = tatCaSan
+                .OrderByDescending(s => tatCaDatSan
+                    .Count(d => d.MaSan == s.MaSan && d.TrangThai != "Đã hủy"))
+                .Take(3)
+                .ToList();
 
             return View(sanNoiBat);
         }
@@ -31,14 +30,12 @@ namespace DATSANBONG.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
