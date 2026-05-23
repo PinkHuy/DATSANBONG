59473a6e030c7c4bc3560ac919bdbd0ab998c1f4
nguyenthanhco0402-sys <nguyenthanhco0402@gmail.com>
2026-05-23 20:13:40 +0700

    hinhanh

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,14 +12,18 @@ namespace DATSANBONG.Controllers
     {
         private readonly DataClasses1DataContext db = new DataClasses1DataContext("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=QuanLySanBong_MVC;Integrated Security=True;MultipleActiveResultSets=True");
 
+        // HomeController.cs
         public ActionResult Index()
         {
-            // Lấy Top 3 sân bóng hoạt động/sẵn sàng có số lượt đặt sân nhiều nhất
-            var sanNoiBat = db.SanBongs
-                .Where(s => s.TrangThai == "Hoạt động" || s.TrangThai == "Sẵn sàng")
-                .OrderByDescending(s => s.DatSans.Count(d => d.TrangThai != "Đã hủy"))
-                .Take(3)
-                .ToList();
+            // ✅ Dùng subquery an toàn hơn cho LINQ to SQL
+            var sanNoiBat = (from s in db.SanBongs
+                             where s.TrangThai == "Hoạt động" || s.TrangThai == "Sẵn sàng"
+                             let soLuotDat = db.DatSans
+                                 .Count(d => d.MaSan == s.MaSan && d.TrangThai != "Đã hủy")
+                             orderby soLuotDat descending
+                             select s)
+                            .Take(3)
+                            .ToList();
 
             return View(sanNoiBat);
         }
